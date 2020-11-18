2b5c88074878b9ffc0ace94c192c03c203c2c9c2
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2020-11-19 02:03:23 +0300

    Revert "Revert "couple fixes""
    This reverts commit 43a9c0d30009b4d735b3420c6e50372131e5cc3f.

--- train.rb
+++ train.rb
@@ -1,74 +1,74 @@
 class Train
-    attr_accessor :speed
-    attr_reader :type; :number; :wagonsCount; :route; :currentStation; :direction
-    def initialize(type, num, wagons,route)
-        @type = type
-        @number = num
-        @wagonsCount = wagons
-        @route = route
-        @speed = 0
-        @currentStation = 0
-        @direction = true # true -> forward. false -> backwards
-        @route.stations[0].trains << self
-    end
-    def wagonsCount
-        @wagonsCount
-    end
-    def currentSpeed
-        @speed
-    end
-    def addWagon
-        if @speed == 0
-            @wagonsCount += 1
-        end
-    end
-    def deleteWagon
-        if @speed == 0
-            @wagonsCount -= 1
-        end
-    end
-    def moveToNextStation
-        
-        @route.stations[@currentStation].trains.delete(self)
-        self.move
-        if @currentStation == @route.stations.length()-1 
-           @direction = !@direction 
-        end
-        if @direction
-            @currentStation += 1
-           
-        else
-            @currentStation -= 1
-        end
-        @route.stations[@currentStation].addTrain(self)
-        if @currentStation == 0
-            @direction = !@direction
-        end
-        self.stop
-    end
-    def move
-        @speed = 80
-    end
-    def stop
-        @speed = 0
-    end
-    def currentStation
-        @route.stations[@currentStation].name
-    end
-    def nextStation
-        if @currentStation == @route.stations.length()-1 
-            @route.stations[@currentStation-1].name
-        else
-            @route.stations[@currentStation+1].name
-        end
-    end
-    def previousStation
-        if @currentStation == 0
-            @route.stations[@currentStation+1].name
-        else
-            @route.stations[@currentStation-1].name
-        end
-    end
+	attr_accessor :speed
+	attr_reader :type; :number; :wagonsCount; :route; :currentStation; :direction
+	def initialize(type, num, wagons,route)
+		@type = type
+		@number = num
+		@wagons_count = wagons
+		@route = route
+		@speed = 0
+		@current_station = 0
+		@direction = true # true -> forward. false -> backwards
+		@route.stations[0].trains << self
+	end
+	def wagons_count
+		@wagons_count
+	end
+	def current_speed
+		@speed
+	end
+	def add_wagon
+		if @speed == 0
+			@wagons_count += 1
+		end
+	end
+	def delete_wagon
+		if @speed == 0
+			@wagons_count -= 1
+		end
+	end
+	def move_to_next_station
+
+		@route.stations[@current_station].trains.delete(self)
+		self.move
+		if @current_station == @route.stations.length-1
+			@direction = !@direction
+		end
+		if @direction
+			@current_station += 1
+
+		else
+			@current_station -= 1
+		end
+		@route.stations[@current_station].add_train(self)
+		if @current_station == 0
+			@direction = !@direction
+		end
+		self.stop
+	end
+	def move
+		@speed = 80
+	end
+	def stop
+		@speed = 0
+	end
+	def current_station
+		@route.stations[@current_station].name
+	end
+	def next_station
+		if @current_station == @route.stations.length-1
+			@route.stations[@current_station-1].name
+		else
+			@route.stations[@current_station+1].name
+		end
+	end
+	def previous_station
+		if @current_station == 0
+			@route.stations[@current_station+1].name
+		else
+			@route.stations[@current_station-1].name
+		end
+	end
 end
 
 
